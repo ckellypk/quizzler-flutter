29739a7f557259c857e99a73e30b6bdee9edfe5c
ckellypk <ckelly@commure.com>
2022-01-18 16:31:13 -0500

    notes complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,39 @@
 import 'package:flutter/material.dart';
-
-
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 /*
-* The starter code just creates a column with containers ot hold the questions
-* We will be adding all of the functionality including a score keeper based on
-* inputs provided by the user button clicks.
 *
-* The scorekeeper will display the answers selected as the user moves through
-* the quiz at the bottom of the screen. We can hard code this now just to see
-* how it will render
+* We should abstract our code and make it more modular. Lets take our quiz app
+* question generation logic out of the UI and stick it in its own file called
+* quiz_brain.dart. We then create a List of questions here and can now access
+* that question list in this page while removing the logic that generated it.
 *
-* Looks good there, now we need to add functionality that keeps track of the
-* answers as well as ask the questions for the quiz. We need to write a function
-* that writes the correct questions to the screen and then processes the
-* answer when one of the true or false buttons are clicked.
+* We don't want to allow the answers to be updated from this file though. We
+* need to restrict its access. By adding an underscore (_) to the front of a
+* property in Dart you make the field private. We make our questionBank private
+* and need to refactor to access the values
 *
-* First lets be able to show the answer provided when the user clicks a button.
-* So we need a row that can hold a list of icons. But we don't want to create
-* that list in the UI as we want to set the state of this list separately so
-* we can update the state easily
+* We make our own getter that gets the string and bool properties from each
+* question depending in the quiz_brain file. We then update this
+* file to know where the values are coming from by accessing the getters.
 *
-* Now that he have our list we can fill it. we can pass in the icons we need but
-* how do we get th screen to update?
+*Now that the quiz brain is in its own file lets move the question counter to
+* that file as well. This makes sense because we don't want any outside code
+* dictating what question we are on in our quiz, and our quiz brain will be
+* completely self contained.
 *
-* The Row widget can take that list and render it to the screen so lets drop our
-* list in there and then update our answer list with the answer input.
+* We crate a private field for this and access it using the nextQuestion method
+* which also validates if there is another question available before updating
+* the question counter and updating the screen.
 *
-* Wee need to add the logic of what needs to happen when each button is pressed
-* here using the onClick property on our button to set the state. We want to add
-* the correct icon with each click
+* Let's also refactor to put the answer checking in a function, that also
+* updates the questions and the score tracker.* Now we have a functioning
+* application that updates based on user inputs and correct and incorrect
+* answers.
 *
-* We also want to display questions based on where they are in the quiz. We need
-* another list of questions that will be referenced when the button is pressed
-* to display a different question to the user. We create the list and update
-* its state in the onPressed property for each button by incrementing the list
-* value displayed by 1.
-*
-* At this point our app updates with the user input as desired, but we still
-* need to set bounds of the application to run only as long as there are
-* questions in the list to ask.
+* Now lets add a package that alerts the user to the end of the quiz. We will
+* use package rFlutter_Alert. I added this to the pubspec.yaml and ran pub get
+* to place it in my project.
 *
 *
 * */
@@ -70,15 +65,50 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
 
+  QuizBrain quizBrain = QuizBrain();
   List<Icon> scoreKeeper = [];
-  List<String> questions= [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
 
-  int questionNumber = 0;
+  void answerChecker(bool answer)  {
+    bool currentCorrectAnswer = quizBrain.getQuestionAnswer();
 
+    if(currentCorrectAnswer == answer){
+      scoreKeeper.add(greenIcon());
+    } else {
+      scoreKeeper.add(redIcon());
+    }
+
+    if(!quizBrain.continueQuiz()){
+      continueAlert().show();
+      setState(() {
+        scoreKeeper = [];
+      });
+    } else {
+      quizBrain.nextQuestion();
+
+    }
+  }
+
+ Alert continueAlert () {
+    return Alert(
+      context: context,
+      type: AlertType.warning,
+      title: "QUIZ COMPLETE",
+      desc: "You have reached the end of the quiz",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "CANCEL",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: ()  {
+            Navigator.pop(context);
+
+          },
+          color: Color.fromRGBO(0, 179, 134, 1.0),
+        ),
+      ]
+    );
+ }
   Icon greenIcon () {
     return Icon(
       Icons.check,
@@ -88,14 +118,10 @@ class _QuizPageState extends State<QuizPage> {
 
   Icon redIcon () {
     return Icon(
-      Icons.check,
+      Icons.close,
       color: Colors.red,
     );
   }
-
-  String currentQuestion = '';
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -109,7 +135,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -134,8 +160,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState( () {
-                  scoreKeeper.add( greenIcon() );
-                  questionNumber++;
+                  answerChecker(true);
                 });
 
               },
@@ -157,8 +182,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 setState( () {
-                  scoreKeeper.add(redIcon());
-                  questionNumber++;
+                  answerChecker(false);
                 });
               },
             ),
